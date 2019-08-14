94dda82132fcdfab14029910b32e31e94b72a540
Orlando Arnosa <oarnosa@gmail.com>
2019-08-14 18:31:21 -0400

    introduced user model

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
 const Campground = require('./models/campground');
 const Comment = require('./models/comment');
+const User = require('./models/user');
 
 mongoose.connect('mongodb://localhost/yelp_camp', {useNewUrlParser: true});
 app.use(bodyParser.urlencoded({extended: true}));
